af8d65b2df74786b4c178a562ed0a70b9fa37e55
Marek Kobida <marek.kobida@gmail.com>
2021-02-14 22:29:13 +0100

    update

--- Router.d.ts
+++
@@ -1,7 +0,0 @@
-import Route from './Route';
-declare class Router<C extends any[]> {
-    routes: Route<C, any>[];
-    addRoute<P extends Partial<Record<string, string>>>(path: string): Route<C, P>;
-    test(context: C, method: string, url: URL | string): Promise<boolean>;
-}
-export default Router;
--- pathToRegExp.d.ts
+++
@@ -1,2 +0,0 @@
-declare function pathToRegExp(path: string): RegExp;
-export default pathToRegExp;
