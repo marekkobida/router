bb482f92f830fd039a07d02ddad2446763c57948
Marek Kobida <marek.kobida@gmail.com>
2021-02-28 11:55:55 +0100

    update

---
+++ pathToRegExp/Parser.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2021 Marek Kobida
+ */
+
+import Lexer from './Lexer';
+
+class Parser {
+  #i: number = 0;
+
+  test(tokens: Lexer.Token[]) {}
+}
+
+export default Parser;
--- pathToRegExp/index.ts
+++ pathToRegExp/index.ts
@@ -3,13 +3,15 @@
  */
 
 import Lexer from './Lexer';
+import Parser from './Parser';
 
 function pathToRegExp(path: string) {
   const lexer = new Lexer();
+  const parser = new Parser();
 
   const tokens = lexer.test(path);
 
-  console.log('tokens', tokens);
+  console.log(parser.test(tokens));
 }
 
 pathToRegExp('/test/:id(\\d+)\\-*');
