25861e23991159ed8fdd32936805d8543756ed34
Marek Kobida <marek.kobida@gmail.com>
2021-03-17 13:02:49 +0100

    up

--- pathToRegExp/index.ts
+++ pathToRegExp/index.ts
@@ -9,11 +9,42 @@ function pathToRegExp(path: string) {
   const lexer = new Lexer();
   const parser = new Parser();
 
-  const tokens = lexer.test(path);
+  const _1 = lexer.test(path);
 
-  console.log(tokens);
+  const _2 = parser.test(_1);
 
-  console.log(parser.test(tokens));
+  console.log('lexer', _1);
+
+  console.log('parser', _2);
+
+  let $ = '';
+
+  $ += '^';
+
+  for (const token of _2) {
+    if (typeof token === 'string') {
+      $ += token;
+    } else {
+      if (token.pattern) {
+        if (token.prefix) {
+          if (token.modifier === '+' || token.modifier === '*') {
+            const mod = token.modifier === '*' ? '?' : '';
+            $ += `(?:${token.prefix}((?:${token.pattern})(?:${token.prefix}(?:${token.pattern}))*))${mod}`;
+          } else {
+            $ += `(?:${token.prefix}(${token.pattern}))${token.modifier}`;
+          }
+        } else {
+          $ += `(${token.pattern})${token.modifier}`;
+        }
+      } else {
+        $ += `(?:${token.prefix})${token.modifier}`;
+      }
+    }
+  }
+
+  $ += '$';
+
+  return new RegExp($);
 }
 
-pathToRegExp('/test/:id(\\d+)');
+const a = pathToRegExp('/test/:id(\\d+)');
