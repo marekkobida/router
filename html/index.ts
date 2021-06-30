bbd9c3ad7f4859fdcb7219833f4cdea57cd8531a
Marek Kobida <marek.kobida@gmail.com>
2021-06-30 16:04:34 +0200

    up

--- html/index.ts
+++ html/index.ts
@@ -6,34 +6,27 @@ import Lexer from '../pathToRegExp/Lexer.js';
 import Parser from '../pathToRegExp/Parser.js';
 import pathToRegExp from '../pathToRegExp/index.js';
 
-function $(elementId: string): HTMLTextAreaElement {
-  return document.getElementById(elementId) as HTMLTextAreaElement;
+function $(id: string): HTMLTextAreaElement {
+  return document.getElementById(id) as HTMLTextAreaElement;
 }
 
 function _1() {
   try {
-    const path = $('path').value;
-    const url = $('url').value;
-
-    const pattern = pathToRegExp(path);
-
-    const _2 = new Lexer().test(path);
-    const _3 = new Parser().test(_2);
+    const path = $('path').value,
+      _2 = new Lexer().test(path),
+      _3 = new Parser().test(_2),
+      pattern = pathToRegExp(path),
+      url = $('url').value;
 
     $('_1').value = JSON.stringify(_2, null, 2);
     $('_2').value = JSON.stringify(_3, null, 2);
     $('parameters').value = JSON.stringify(pattern.exec(url), null, 2);
     $('pattern').value = pattern.toString();
   } catch (error) {
-    $('_1').value = error;
-    $('_2').value = error;
-    $('parameters').value = error;
-    $('pattern').value = error;
+    ['_1', '_2', 'parameters', 'pattern'].forEach(id => ($(id).value = error));
   }
 }
 
-const elements = [$('path'), $('url')];
-
-elements.forEach(element => element.addEventListener('keyup', _1));
+['path', 'url'].forEach(id => $(id).addEventListener('keyup', _1));
 
 _1();
--- public/html/index.js
+++ public/html/index.js
@@ -4,28 +4,20 @@
 import Lexer from '../pathToRegExp/Lexer.js';
 import Parser from '../pathToRegExp/Parser.js';
 import pathToRegExp from '../pathToRegExp/index.js';
-function $(elementId) {
-    return document.getElementById(elementId);
+function $(id) {
+    return document.getElementById(id);
 }
 function _1() {
     try {
-        var path = $('path').value;
-        var url = $('url').value;
-        var pattern = pathToRegExp(path);
-        var _2 = new Lexer().test(path);
-        var _3 = new Parser().test(_2);
+        var path = $('path').value, _2 = new Lexer().test(path), _3 = new Parser().test(_2), pattern = pathToRegExp(path), url = $('url').value;
         $('_1').value = JSON.stringify(_2, null, 2);
         $('_2').value = JSON.stringify(_3, null, 2);
         $('parameters').value = JSON.stringify(pattern.exec(url), null, 2);
         $('pattern').value = pattern.toString();
     }
     catch (error) {
-        $('_1').value = error;
-        $('_2').value = error;
-        $('parameters').value = error;
-        $('pattern').value = error;
+        ['_1', '_2', 'parameters', 'pattern'].forEach(function (id) { return ($(id).value = error); });
     }
 }
-var elements = [$('path'), $('url')];
-elements.forEach(function (element) { return element.addEventListener('keyup', _1); });
+['path', 'url'].forEach(function (id) { return $(id).addEventListener('keyup', _1); });
 _1();
