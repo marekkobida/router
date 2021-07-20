eb4ab3bf2487216230ab7165a36a2b396d5e0342
Marek Kobida <marek.kobida@gmail.com>
2021-07-20 14:15:22 +0200

    deleted public

--- public/test/isNameValid.d.ts
+++
@@ -1,3 +0,0 @@
-import Route from '../Route.js';
-declare const isNameValid: Route.AfterTest;
-export default isNameValid;
--- public/test/isNameValid.js
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright 2021 Marek Kobida
- */
-const isNameValid = ({ urlParameters: [name] }, next) => {
-    if (name && /^[a-z]+$/.test(name))
-        return next();
-    throw new Error('The name is not valid.');
-};
-export default isNameValid;
